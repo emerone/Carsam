e2897914aa09b646caf85275dde4fb988e66ce9d
Emmanuel <emmanuel.lebaramo@gmail.com>
2022-10-06 17:03:53 +0200

    Update ( Catalogue )

--- assets/js/Hash.js
+++ assets/js/Hash.js
@@ -1,15 +1,11 @@
-if ( window.location.hash == "#Catalogue"){
+while ( window.location.hash == "#Catalogue"){
 
-    // !        Without animation 
-
-    body.setAttribute("class", "b")
+    document.querySelector('body').setAttribute("class", "b")
 
 }
 
-if ( window.location.hash == "#%C3%80-propos"){
+while ( window.location.hash == "#%C3%80-propos"){
 
-    // !        Without animation 
-
-    body.setAttribute("class", "c")
+    document.querySelector('body').setAttribute("class", "c")
 
 }
